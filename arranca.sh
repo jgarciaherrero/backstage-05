4fe3201d843d27d35e4e5e64be9e3d7d9bcf5536
jgarciaherrero <javigh73@gmail.com>
2023-10-30 15:35:52 +0100

    GitHub: integration

---
+++ arranca.sh
@@ -0,0 +1,10 @@
+echo nvm use 18.18.2
+export POSTGRESQL_HOST=localhost
+export POSTGRESQL_PORT=5432
+export POSTGRESQL_USER=postgres
+export POSTGRESQL_PASSWORD=secret
+
+export GITHUB_TOKEN=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
+
+yarn dev
+
